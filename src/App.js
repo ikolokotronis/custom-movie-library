60dd821183480a209a3c5059fd76d7a90dec8af8
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-04-12 14:59:06 +0200

    search component style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {VStack} from "@chakra-ui/react";
 import {Search} from "./components/Search";
+import {MovieList} from "./components/MovieList";
 
 function App() {
   return (
       <VStack>
-        <Search/>
+          <Search/>
+          <MovieList/>
       </VStack>
   );
 }
